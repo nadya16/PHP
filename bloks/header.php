2ee5e808553884f0ea8d8e7b8084424225112203
nadya16 <nadya97vas@yandex.ru>
2017-11-01 19:07:55 +0400

    Коммит1

---
+++ bloks/header.php
@@ -0,0 +1,19 @@
+<!-- начало хедера -->
+<div class = "center" vid="header" align ="center">
+<!-- между метками HHH и DDD будет наполнение хедера -->
+<!-- HHH -->
+
+
+<!--
+<table width="100%" height="100px" border="0" cellspacing="0">
+  <tr>
+<td align="center"><img  src="img/logo_garage.png" alt=""></td>
+<td align="center"><img src="img/header.jpg" width="700px"alt=""></td>
+</tr>
+</table>
+-->
+<img align="center" src="img/header.jpg" alt="">
+
+<!-- DDD -->
+</div><!-- #header-->
+<!-- конец хедера -->
